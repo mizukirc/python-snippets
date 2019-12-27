ed96080b9fa2ad0fe551a3a9c5a7504d1fc12915
H2O <mizuki.job@gmail.com>
2019-12-27 17:01:50 +0900

    Update divmod_ex.py

--- builtin_functions/divmod_ex.py
+++ builtin_functions/divmod_ex.py
@@ -1,9 +1,14 @@
 '''
 [doc: divmod](https://docs.python.org/3/library/functions.html#divmodhttps://docs.python.org/3/library/functions.html#divmod)
-- Syntax
-```divmod(dividend, divisor)```
-- Returns
-tuple
-- Example: 
+
+### Syntax
+```divmod(a, b)```
+
+### Returns
+tuple 
+(a // b, a % b)
+(商, 余り)
+
+### Example: 
 '''
 
