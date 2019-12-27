abb4f18c242a93e921a34d88fdb4088060b4fd48
H2O <mizuki.job@gmail.com>
2019-12-27 16:57:17 +0900

    Create divmod_ex.py

---
+++ builtin_functions/divmod_ex.py
@@ -0,0 +1,9 @@
+'''
+[doc: divmod](https://docs.python.org/3/library/functions.html#divmodhttps://docs.python.org/3/library/functions.html#divmod)
+- Syntax
+```divmod(dividend, divisor)```
+- Returns
+tuple
+- Example: 
+'''
+
