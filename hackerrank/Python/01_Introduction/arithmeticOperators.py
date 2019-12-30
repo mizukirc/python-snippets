3fe1604894366c8bb1c95427ee37ec465e0460d2
H2O <mizuki.job@gmail.com>
2019-12-30 23:16:41 +0900

    Add files via upload

---
+++ hackerrank/Python/01_Introduction/arithmeticOperators.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# https://www.hackerrank.com/challenges/python-arithmetic-operators/
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
