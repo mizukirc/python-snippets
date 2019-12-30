e45da46843be159e3c469e603cfa4c2b85f1f95e
H2O <mizuki.job@gmail.com>
2019-12-30 22:58:00 +0900

    Add files via upload

---
+++ hackerrank/Python/01_Introduction/printFunction.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# https://www.hackerrank.com/challenges/python-print/
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i, end='')
+
