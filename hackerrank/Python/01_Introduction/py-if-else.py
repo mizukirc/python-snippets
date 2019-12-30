01c09bf314da9eca75192d0c30f0c022a5d87415
H2O <mizuki.job@gmail.com>
2019-12-30 19:57:26 +0900

    Update py-if-else.py

--- hackerrank/Python/01_Introduction/py-if-else.py
+++ hackerrank/Python/01_Introduction/py-if-else.py
@@ -11,13 +11,13 @@ import sys
 
 if __name__ == '__main__':
     n = int(input().strip())
-    a,b = divmod(n,2)
-    if(b==1):
+    a,b = divmod(n, 2)
+    if(b == 1):
         print("Weird")
-    elif(b==0):
-        if(2<=n<=5) or (n>20):
+    elif(b == 0):
+        if(2 <= n <= 5) or (n > 20):
             print("Not Weird")
-        elif(6<=n<=20):
+        elif(6 <= n <= 20):
             print("Weird")
         
     
