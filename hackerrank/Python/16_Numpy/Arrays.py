8e010d7a26c4e145b436423515e8885a9e97cec5
mizu <mizuki.job@gmail.com>
2020-01-04 13:56:18 +0900

    Add files via upload

--- hackerrank/Python/16_Numpy/Arrays.py
+++ hackerrank/Python/16_Numpy/Arrays.py
@@ -1,1 +1,15 @@
+# https://www.hackerrank.com/challenges/np-arrays/
+#!/bin/python3
+
+import numpy
+
+def arrays(arr):
+    result = numpy.array(arr, float)
+    result = result[::-1]
+    return result
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
+
 
