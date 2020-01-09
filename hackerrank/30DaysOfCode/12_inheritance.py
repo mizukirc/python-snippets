c62ac5704f34a903d932704c8f95af3e3ed402dc
mizu <mizuki.job@gmail.com>
2020-01-09 12:25:43 +0900

    Add files via upload

--- hackerrank/30DaysOfCode/12_inheritance.py
+++ hackerrank/30DaysOfCode/12_inheritance.py
@@ -42,3 +42,13 @@ class Student(Person):
         else:
             grade = 'T'
         return grade
+    
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input()) # not needed for Python
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
