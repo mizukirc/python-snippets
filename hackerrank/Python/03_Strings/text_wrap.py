a098760da340908cb66f7e78883d998dfd2751c3
mizu <mizuki.job@gmail.com>
2020-01-09 09:50:46 +0900

    Update text_wrap.py

--- hackerrank/Python/03_Strings/text_wrap.py
+++ hackerrank/Python/03_Strings/text_wrap.py
@@ -1,11 +1,11 @@
 import textwrap
 
 def wrap(string, max_width):
-    result = textwrap.wrap(string, max_width)
+    wrap_list = textwrap.wrap(string, max_width)
+    result = '\n'.join(wrap_list)
     return result
 
 if __name__ == '__main__':
     string, max_width = input(), int(input())
-    wrap_list = wrap(string, max_width)
-    result = '\n'.join(wrap_list)
+    result = wrap(string, max_width)
     print(result)
