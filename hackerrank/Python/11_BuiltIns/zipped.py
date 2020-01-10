b416edaa20aaa0c17c93a7ff4c9de708cbf5e14b
mizu <mizuki.job@gmail.com>
2020-01-10 09:23:03 +0900

    Add files via upload

--- hackerrank/Python/11_BuiltIns/zipped.py
+++ hackerrank/Python/11_BuiltIns/zipped.py
@@ -1,1 +1,14 @@
+in_size = input().strip().split(' ')
+in_size = list(map(int, in_size))
+scores = list()
 
+for i in range(in_size[1]):
+    scores_horizontal = input().strip().split(' ')
+    scores_horizontal = list(map(float, scores_horizontal))
+    scores.append(scores_horizontal)
+    
+scores_T = list( zip(*scores) )
+scores_list = list(map(sum, scores_T))
+result_list = list(map(lambda x: x/in_size[1], scores_list))
+result = [round(result_list[i],2) for i in range(len(result_list))]
+[print(i) for i in result]
