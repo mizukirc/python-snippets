702b513eee911b7972b616326d6e0765ba1aa7ab
mizu <mizuki.job@gmail.com>
2020-01-23 18:34:42 +0900

    Add files via upload

--- hackerrank/Python/06_Itertools/itertools_product.py
+++ hackerrank/Python/06_Itertools/itertools_product.py
@@ -1,1 +1,6 @@
+from itertools import product
 
+A = tuple(map(int, input().split()))
+B = tuple(map(int, input().split()))
+lst = list(product(A, B))
+[print(lst[i], end=' ') for i in range(len(lst))]
