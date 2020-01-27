e74280285890f0ff612806a133475dd4513ef90a
mizu <mizuki.job@gmail.com>
2020-01-27 17:47:28 +0900

    Add files via upload

--- hackerrank/Python/07_Collections/collections_counter.py
+++ hackerrank/Python/07_Collections/collections_counter.py
@@ -1,1 +1,15 @@
+from collections import Counter
 
+X = int(input())
+shoe_size = list(map(int, input().split()))
+N = int(input())
+
+c = Counter(shoe_size)
+
+totalval = 0
+for i in range(N):
+    size, val = map(int, input().split())
+    if c[size] != 0:
+        totalval = totalval + val
+        c[size] = c[size]-1
+print(totalval)
