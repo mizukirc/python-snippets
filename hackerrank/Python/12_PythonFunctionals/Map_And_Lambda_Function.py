cbbb76197c373c8e899a4d969a97faac9f5d3a31
mizu <mizuki.job@gmail.com>
2020-02-04 11:13:30 +0900

    Add files via upload

--- hackerrank/Python/12_PythonFunctionals/Map_And_Lambda_Function.py
+++ hackerrank/Python/12_PythonFunctionals/Map_And_Lambda_Function.py
@@ -1,1 +1,15 @@
+cube = lambda x: pow(x,3)
 
+def fibonacci(n):
+    if n >= 0:
+        lst = [0]
+    if n >= 1:
+        lst.append(1)
+    if n >= 2:
+        for i in range(2,n+1):
+            lst.append(lst[-1]+ lst[-2]) 
+    return lst[:n]
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
