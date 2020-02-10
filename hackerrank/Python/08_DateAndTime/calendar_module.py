0b1d748eea9a1e67adc993399d085dc6971089de
mizu <mizuki.job@gmail.com>
2020-02-10 11:45:48 +0900

    Add files via upload

--- hackerrank/Python/08_DateAndTime/calendar_module.py
+++ hackerrank/Python/08_DateAndTime/calendar_module.py
@@ -1,1 +1,13 @@
+import calendar 
+month, day, year = map(int, input().split())
 
+days = {
+    0: 'MONDAY',
+    1: 'TUESDAY',
+    2: 'WEDNESDAY',
+    3: 'THURSDAY',
+    4: 'FRIDAY',
+    5: 'SATURDAY',
+    6: 'SUNDAY'
+}
+print(days[calendar.weekday(year, month, day)])
